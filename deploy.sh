c08d0b2af8f1edee277a8107b663f615e5b8099d
Jón Trausti <icewolfy@gmail.com>
2020-04-17 08:36:58 +0000

    Fixed cache-control header and added live config

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ yarn run build
 echo Copying files to S3
 aws s3 sync ./public s3://$1/blog --exclude "*.html" --exclude "*.jpg" --exclude "*.png" --exclude "*.css" --exclude "*.js" --delete --exclude "blog/*" --cache-control max-age=1314000,public
 aws s3 cp ./public s3://$1/blog/ --exclude "*" --include "*.jpg" --recursive --include "*.png" --include "*.css" --include "*.js" --metadata-directive REPLACE --acl public-read --cache-control max-age=15778476,public
-aws s3 cp ./public s3://$1/blog/ --exclude "*" --include "*.html" --recursive --metadata-directive REPLACE --cache-control public,s-maxage=600,max-age=10,must-revalidate --content-type text/html --acl public-read
+aws s3 cp ./public s3://$1/blog/ --exclude "*" --include "*.html" --recursive --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-type text/html --acl public-read
 echo Invalidating CloudFront cache
 aws cloudfront create-invalidation --distribution-id $2 --paths "/blog*"
 echo Deployment steps finished
